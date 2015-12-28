e8973b1c58c693b923fb9e09daee189f370a0bed
Farzaneh Bakhshaei <farzaneh.bakhshaei@stud.tu-darmstadt.de>
2015-12-28 21:11:53 +0100

    Update stringTimes.java

--- CodingBat/Warmup-2/stringTimes.java
+++ CodingBat/Warmup-2/stringTimes.java
@@ -1,3 +1,4 @@
+// Given a string and a non-negative int n, return a larger string that is n copies of the original string. 
 public String stringTimes(String str, int n) {
   String temp = "";
   for (int i=0; i<n; i++) {
