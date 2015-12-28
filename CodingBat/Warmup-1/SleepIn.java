b780dbd90d4f7589d70940f0578e4c1e80c5fa0d
FarzanehBakhshaei <bakhshaei.farzaneh67@gmail.com>
2015-12-28 20:25:05 +0100

    Update SleepIn.java

--- CodingBat/Warmup-1/SleepIn.java
+++ CodingBat/Warmup-1/SleepIn.java
@@ -1,3 +1,4 @@
+// Given a string and a non-negative int n, return a larger string that is n copies of the original string. 
 public boolean sleepIn(boolean weekday, boolean vacation) {
   if (!weekday || vacation) {
   return true;
