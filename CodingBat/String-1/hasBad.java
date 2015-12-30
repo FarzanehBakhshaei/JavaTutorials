a58defa9af146e77b453b26df6b486c715a6c677
Farzaneh Bakhshaei <farzaneh.bakhshaei@stud.tu-darmstadt.de>
2015-12-30 04:15:22 +0100

    Create hasBad.java

---
+++ CodingBat/String-1/hasBad.java
@@ -0,0 +1,12 @@
+// Given a string, return true if "bad" appears starting at index 0 or 1 in the string, 
+// such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, including 0.
+public boolean hasBad(String str) {
+  if(str.length() < 3) {
+     return false;
+  }
+  if(str.length() == 3){
+     return str.substring(0, 3).equals("bad");
+  }
+  return str.substring(0, 3).equals("bad") || 
+         str.substring(1, 4).equals("bad");
+}
