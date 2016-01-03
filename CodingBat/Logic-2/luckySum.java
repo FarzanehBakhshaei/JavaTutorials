6403c9f3c4367798f312dde662d2ed33d77e7120
Farzaneh Bakhshaei <farzaneh.bakhshaei@stud.tu-darmstadt.de>
2016-01-03 17:57:19 +0100

    Create luckySum.java

---
+++ CodingBat/Logic-2/luckySum.java
@@ -0,0 +1,13 @@
+// Given 3 int values, a b c, return their sum. 
+// However, if one of the values is 13 then it does not count towards the sum and values to its right do not count.
+// So for example, if b is 13, then both b and c do not count. 
+public int luckySum(int a, int b, int c) {
+  int sum = 0;
+  if (a==13) return sum;
+  sum += a;
+  if (b==13) return sum;
+  sum += b;
+  if (c==13) return sum;
+  sum += c;
+  return sum;
+}
